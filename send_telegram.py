c6a068809a479e706242ab4adfe6329f6cc777b8
lilu <23082010qQ>
2021-12-20 23:14:09 +0300

    Fix function publish photo to Telegram channel

--- send_telegram.py
+++ send_telegram.py
@@ -1,12 +1,11 @@
-import requests
 import os.path
 from os import listdir
+import telegram
 from dotenv import load_dotenv
 import time
 import random
 
 load_dotenv()
-
 tg_token = os.getenv('TG_TOKEN')
 tg_chat_id = os.getenv('TG_CHAT_ID')
 channel_id = os.getenv('CHANEL_ID')
@@ -16,12 +15,9 @@ sleep_time = 86400
 def send_telegram():
     while True:
         photos_list = listdir('images')
-        url = f'https://api.telegram.org/bot{tg_token}/sendPhoto'
 
         with open(f'images/{random.choice(photos_list)}', 'rb') as file:
-            files = {'photo': file}
-            response = requests.post(url, data={'chat_id': channel_id}, files=files)
+            bot = telegram.Bot(token=tg_token)
+            bot.send_photo(chat_id=tg_chat_id,
+                           photo=file)
             time.sleep(sleep_time_test)
-
-        if not response.ok:
-            raise Exception('post_text error')
