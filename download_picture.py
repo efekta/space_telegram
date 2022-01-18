9136e1b4dcfb20e0da18bc55e524b33b10b5dd32
lilu <23082010qQ>
2022-01-18 20:24:59 +0300

    An optional argument can be set for the function by specifying a default value for it - default value

--- download_picture.py
+++ download_picture.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def download_picture(url: str, payload, path: str):
+def download_picture(url: str,  path: str, payload=''):
     response = requests.get(url, params=payload)
     response.raise_for_status()
 
